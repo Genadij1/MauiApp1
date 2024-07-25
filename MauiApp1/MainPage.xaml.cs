4fd3189f87ac3572164495776c153cca44fd8916
Genadij1 <bgennadij265@icloud.com>
2024-07-25 20:08:51 +0300

    Добавьте файлы проекта.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnLoadButtonClicked(object sender, EventArgs e)
+        {
+            string url = "https://www.gutenberg.org/files/1524/1524-0.txt";
+            string hamletText = await LoadTextFromUrl(url);
+            contentEditor.Text = hamletText;
+        }
+
+        private async Task<string> LoadTextFromUrl(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    return responseBody;
+                }
+                catch (HttpRequestException e)
+                {
+                    await DisplayAlert("Request Error", e.Message, "OK");
+                    return string.Empty;
+                }
+            }
+        }
+    }
+}
